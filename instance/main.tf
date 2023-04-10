817cd33756b1da04936b7ab2f4bf0e40f6d88061
akmara1 <akmaralnurbekova03@gmial.com>
2023-04-10 14:01:10 +0600

    bastion sg

--- instance/main.tf
+++ instance/main.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "bastion-sg" {
   name   = "bastion-security-group"
-  vpc_id = "${module.vpc.vpc_id}"
+  vpc_id = var.vpc_id
 
   ingress {
     protocol    = "tcp"
@@ -22,7 +22,7 @@ resource "aws_instance" "bastion" {
   user_data = base64encode(file("user_data.sh"))
   subnet_id                   = var.subnet_id
   instance_type               = var.instance_type
-  vpc_security_group_ids      = [aws_security_group.bastion_sg]
+  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
   associate_public_ip_address = true
 
   tags = {
--- instance/var.tf
+++ instance/var.tf
@@ -2,10 +2,6 @@ variable "ami" {
   type        = string
   description = "AMI to use for the instance."
 }
-variable "bastion_sg" {
-  type        = string
-  description = "List of security group names to associate with"
-}
 variable "instance_type" {
   type        = string
   description = "Instance type to use for the instance"
@@ -17,4 +13,7 @@ variable "subnet_id" {
 variable "project" {
   type = string
   description = "environment name"
+}
+variable "vpc_id" {
+  
 }
